b9f3e4e65edf3733343831773c0454cafcfe6b99
Henry Jonathan <henryjonathanchandra12@gmail.com>
2024-08-15 09:37:50 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="mx-24">
+        <div class="flex font-medium text-lg gap-5 mt-10">
+            <a href="">Home</a>
+            <a href="">Beasiswa</a>
+        </div>
+        <div class="bg-sky-600 p-20 flex items-center justify-center mt-5 rounded-md shadow-lg">
+            <h1 class="text-2xl font-semibold text-white">Selamat datang di website Testing</h1>
+        </div>
+        <div class="grid grid-cols-5 mt-10 gap-8">
+            <div class="bg-gray-200 rounded-md shadow-lg p-20">
+                <h3 class="text-lg font-semibold"></h3>
+            </div>
+            <div class="bg-gray-200 rounded-md shadow-lg p-20">
+                <h3 class="text-lg font-semibold"></h3>
+            </div>
+            <div class="bg-gray-200 rounded-md shadow-lg p-20">
+                <h3 class="text-lg font-semibold"></h3>
+            </div>
+            <div class="bg-gray-200 rounded-md shadow-lg p-20">
+                <h3 class="text-lg font-semibold"></h3>
+            </div>
+            <div class="bg-gray-200 rounded-md shadow-lg py-20">
+                <h3 class="text-lg font-semibold"></h3>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
